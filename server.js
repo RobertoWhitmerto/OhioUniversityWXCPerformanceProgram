8ec8c219979c5b4101a781822b46b99fb3522a1e
Peter Essman <pe017014@ohio.edu>
2016-09-27 19:40:49 -0400

    Changed index.js to server.js and added simple html display to root page of node.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname+'/signin.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
