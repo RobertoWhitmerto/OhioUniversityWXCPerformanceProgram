b547632980247957cb79c128feb969862b2a98c5
Robert Whitmer <robbie.whitmer@gmail.com>
2016-09-28 16:41:34 -0400

    changed .ebignore to include .sh files and added test of release script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Error: Usage: ./deploy.sh \"COMMIT MESSAGE\""
+fi
+
+commit=$1
+gcommand="git commit -m "\""$commit"\"
+echo $gcommand
+
+git add .
+
+eval $gcommand
