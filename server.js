4028a29d6f61aaa25924373e2652c393f3aff50e
Peter Essman <pe017014@ohio.edu>
2016-09-28 15:07:57 -0400

    updated code for parsing user input and added another html page

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 var express = require('express');
-var app = express();
 var path = require("path");
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname+'/signin.html'));
+  response.sendFile(path.join(__dirname+'/site.htm'));
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.post("/", function(req, res){
+  console.log(req.body.user)
+  console.log(req.body.pass)
 });
 
 app.listen(app.get('port'), function() {
