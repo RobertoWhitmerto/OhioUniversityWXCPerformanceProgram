fa207a98fa3cadbf7a415d7d40d694d39c7ba1a7
Peter Essman <pe017014@ohio.edu>
2016-09-28 18:05:32 -0400

    commented some updates to revert to previous versions functionality

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 var express = require('express');
 var path = require("path");
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+//var bodyParser = require('body-parser');
+//var cookieParser = require('cookie-parser');
 //var db = require('./db');
 
 var app = express();
-
+/*
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+*/
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/site.htm'));
 });
-
+/*
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.post("/", function(req, res){
   console.log(req.body.user)
   console.log(req.body.pass)
 });
-
+*/
 /*var queryString = 'SELECT * FROM Roles;';
 db.query(queryString, function(err, rows, fields) {
     if (err) throw err;
