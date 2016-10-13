079a17fabdbab4ba89b6b4c82ee9557a607cc963
Robert Whitmer <robbie.whitmer@gmail.com>
2016-10-13 18:21:41 -0400

    Added checking user login against db user table and other housekeeping

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require("path");
 //var logger = require('morgan');
 var bodyParser = require('body-parser');
 //var cookieParser = require('cookie-parser');
-//var db = require('./db');
+var db = require('./db');
 
 var app = express();
 
@@ -15,19 +15,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 */
+app.use(express.static(__dirname + '/'));
 app.use(express.static('materialize'));
 app.set('port', (process.env.PORT || 5000));
 
+
+//ROUTES
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/site.htm');
 });
 
+app.get('/signin', function(request, response) {
+  response.sendFile(__dirname + '/signin.html');
+});
 
+app.get('/workoutentry', function(request, response) {
+  response.sendFile(__dirname + '/workoutentry.htm');
+});
+
+//BODY PARSER
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.post("/", function(req, res){
   console.log(req.body.user)
   console.log(req.body.pass)
+  var user = req.body.user;
+  var pass = req.body.pass;
+  /*var queryString = `INSERT INTO ouwxcpp_db.user(username, email, password, create_time)  						VALUES  ("${user}", "robbie.whitmer@gmail.com", "${pass}", NOW())`;
+  db.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+  });
+*/
+  var queryString = `SELECT * FROM ouwxcpp_db.user WHERE username="${user}"`;
+  db.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+    if (rows.length > 0){
+    	if(pass === rows[0].password){
+    		console.log('Correct password!');
+  			  res.redirect('/signin.html');
+    		console.log('Did we make it here?');
+    	}
+    	else
+    		console.log('Incorrect Password');
+    }
+    else
+    	console.log('Not a user');
+  });
 });
 
 /*var queryString = 'SELECT * FROM Roles;';
@@ -39,6 +72,8 @@ db.query(queryString, function(err, rows, fields) {
 });
 */
 
+
+// LAUNCH
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
