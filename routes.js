1d2d17dbecc997b652da929a4386d7d09f18ba40
Peter Essman <pe017014@ohio.edu>
2016-10-24 17:25:04 -0400

    separated routes of add user and remove user

--- routes.js
+++ routes.js
@@ -23,8 +23,8 @@ app.get('/admin_add_user', function(request, response) {
 	response.sendFile(__dirname + '/admin_add_user.htm');
 });
 
-app.get('/site', function(request, response) {
-	response.sendFile(__dirname + '/site.htm');
+app.get('/admin_remove_user', function(request,response) {
+	response.sendFile(__dirname + '/admin_remove_user.htm');
 });
 
 };
