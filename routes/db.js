ffcfb576139a8d15867ff16eaafb34932d3ed908
Peter Essman <pe017014@ohio.edu>
2016-11-03 09:41:10 -0400

    added new directory to hold our routes

---
+++ routes/db.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'ouwxc.cevwncmpvhyl.us-west-2.rds.amazonaws.com',
+    user     : 'cs4560',
+    password : 'teamonacob',
+    database : 'OUWXC'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
