43092bd97fd7ba3884cf36834a3122a8cf46b6f1
Peter Essman <pe017014@ohio.edu>
2016-12-07 20:46:08 -0500

    removed '}' at 241 that was causing error

--- routes/index.js
+++ routes/index.js
@@ -237,7 +237,7 @@ passport.deserializeUser(function(id, done){
 		done(null, result);
 	})
 });
-}
+
 router.post('/',
 	passport.authenticate('local'), function(req, res){
 		console.log(req.user);
