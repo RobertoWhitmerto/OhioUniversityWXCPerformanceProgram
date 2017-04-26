c96ab2543e498b034950792e21022f3a46a0d67e
Robert Whitmer <robbie.whitmer@gmail.com>
2017-04-26 16:51:48 -0400

    updated db.js with new client server info

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host     : 'ouwxc.cevwncmpvhyl.us-west-2.rds.amazonaws.com',
+    host     : 'ohioperformance.cwiki2unmymf.us-west-2.rds.amazonaws.com',
     user     : process.env.username,
     password : process.env.password,
-    database : 'PPA'
+    database : 'OhioPerformanceProgram'
 });
 
 connection.connect(function(err) {
