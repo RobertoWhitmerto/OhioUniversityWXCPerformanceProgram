7c66edd6ad47c9d0805e87c687d6c07604430ed5
Robert Whitmer <robbie.whitmer@gmail.com>
2017-04-26 17:11:48 -0400

    removed the db require from server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ var flash = require('connect-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var db = require('./db');
 
 var routes = require('./routes/index');
 
